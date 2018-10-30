daa9490c3ebededea5fdbd4500472dc9f4e7e279
minhthai <minhthai40@gmail.com>
2018-10-30 22:22:37 +0800

    Add binary search

--- src/main/scala/BinarySearch.scala
+++ src/main/scala/BinarySearch.scala
@@ -1,6 +1,28 @@
+import scala.annotation.tailrec
+
+/**
+  * Returns the index of `x` in the given sorted array `arr`.
+  * If `x` is not found, returns the index where `x` should be inserted
+  * so that `arr` remains sorted.
+  */
 class BinarySearch {
 
   def run(arr: Array[Int], x: Int): Int = {
-    1
+    binSearch(arr, x, 0, arr.length - 1)
+  }
+
+  @tailrec
+  final def binSearch(arr: Array[Int], x: Int, low: Int, hi: Int): Int = {
+    if(hi < low) {
+      low
+    }
+    else {
+      val pivot = (low + hi) / 2
+      arr(pivot) match {
+        case a if a == x => pivot
+        case a if x < a  => binSearch(arr, x, low, pivot - 1)
+        case _           => binSearch(arr, x, pivot + 1, hi)
+      }
+    }
   }
 }
--- src/test/scala/BinarySearchSuite.scala
+++ src/test/scala/BinarySearchSuite.scala
@@ -7,5 +7,12 @@ class BinarySearchSuite extends SolutionSuite {
   }
 
   testCase(Array(1, 2, 3), 2, expect = 1)
+  testCase(Array(1, 2, 3, 4), 2, expect = 1)
+  testCase(Array(1, 2, 3, 4), 3, expect = 2)
+  testCase(Array(1, 2, 3, 4), 4, expect = 3)
+  testCase(Array(), 4, expect = 0)
+  testCase(Array(1, 2, 10, 20), -5, expect = 0)
+  testCase(Array(1, 2, 10, 20), 12, expect = 3)
+  testCase(Array(1, 2, 10, 20), 30, expect = 4)
 
 }
